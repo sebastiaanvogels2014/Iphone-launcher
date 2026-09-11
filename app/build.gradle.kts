c4ab3d5183c36bf094ba4b74721bfc29bbb5b1a0
sebastiaanvogels2014 <sebastiaanvogels2014@gmail.com>
2026-09-11 22:05:48 +0200

    Add launcher app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins { id("com.android.application") }
+
+android { namespace = "nl.sebastiaan.iphonelauncher"; compileSdk = 35
+    defaultConfig { applicationId = "nl.sebastiaan.iphonelauncher"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+}
